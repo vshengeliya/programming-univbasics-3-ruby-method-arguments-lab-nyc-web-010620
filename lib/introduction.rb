85ceb917b67a660b1d7b7eeee420ceae2a1817a8
Veronika Dodda <v.shengeliya@gmail.com>
2020-01-04 03:16:26 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,15 +1,19 @@
-name = "Maria", language = "Python"
+
 def introduction (name)
-puts "Hi, my name is #{name}"
+puts "Hi, my name is #{name}."
 end
+
 
 def introduction_with_language (name, language)
-puts "Hi, my name is #{name} and I am leaning to program in #{language}"
+puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
 
-def introduction_with_language_optional (name, language = "Ruby")
-puts "Hi, my name is #{name} and I am leaning to program in #{language}"
+
+
+def introduction_with_language_optional (name , language = "Ruby")
+puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+
 
 
 
