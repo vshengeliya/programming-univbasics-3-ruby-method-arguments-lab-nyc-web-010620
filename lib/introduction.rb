b6a59641b11d91e07df55c975c2c1808aed4a660
Veronika Dodda <v.shengeliya@gmail.com>
2020-01-04 03:08:22 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,15 +1,22 @@
-name = "Maria", language = "Python"
+
 def introduction (name)
 puts "Hi, my name is #{name}"
 end
+
+introduction("Dan")
 
 def introduction_with_language (name, language)
 puts "Hi, my name is #{name} and I am leaning to program in #{language}"
 end 
 
-def introduction_with_language_optional (name, language = "Ruby")
+introduction_with_language ("Josh", "React")
+
+def introduction_with_language_optional (name , language = "Ruby")
 puts "Hi, my name is #{name} and I am leaning to program in #{language}"
 end
+
+introduction_with_language_optional("Edwin", "Ruby")
+introduction_with_language_optional("Edwin", "Phyton")
 
 
 
