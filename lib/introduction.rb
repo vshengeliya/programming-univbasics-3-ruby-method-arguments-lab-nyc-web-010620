80a24df4bb65f6917053d5dbe314fb9440403f00
Veronika Dodda <v.shengeliya@gmail.com>
2020-01-04 03:15:31 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,7 +5,7 @@ end
 
 
 def introduction_with_language (name, language)
-puts "Hi, my name is #{name} and I am leaning to program in #{language}."
+puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
 
 
